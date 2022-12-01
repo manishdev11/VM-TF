9b44d49298d637a66e8740c8e7d14a8aca055972
manishdev11 <111513917+manishdev11@users.noreply.github.com>
2022-12-01 13:58:39 +0530

    Update VM.tf

--- VM.tf
+++ VM.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "b46e91d4-0a6d-4fb9-b966-52851b7d7c0b"
-  client_id = "9f7eab5d-6ab1-4397-8c67-76d3dbe06ad9"   // Application (client) ID
-  client_secret = "Yay8Q~d1gjZaoHlNt5IstzbUi-rOiAbqCuk7jbyt"  // secret value
-  tenant_id = "e4cdd77c-c5aa-4b73-a842-1ef34b53825e"   // Directory (tenant) ID
+  subscription_id = "b46e91d4-0a6d-4fb9-b966-5285bn7d7c0b"
+  client_id = "9f7eab5d-6ab1-4397-8c67-76d3dbe16ad9"   // Application (client) ID
+  client_secret = "Yay8Q~d1gjZaoHlNt5IutzbUi-r8iAbqCuk7jbyt"  // secret value
+  tenant_id = "e4cdd77c-c5aa-4b73-a842-1ef3nb53825e"   // Directory (tenant) ID
   features {}
 }
 resource "azurerm_resource_group" "rg-tf" {
